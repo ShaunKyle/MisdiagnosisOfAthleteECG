fbee525488f32d2f3b07ae062ff37de734a6e7e8
Shaun Kyle <shaun@jpk.co.nz>
2025-04-01 16:40:05 +1100

    modify driver to work with any wfdb record (not just .mat data files)

--- PhysioNet2020_driver.py
+++ PhysioNet2020_driver.py
@@ -1,31 +1,24 @@
 from pathlib import Path
 import numpy as np, os, sys
-from scipy.io import loadmat
+from wfdb import rdrecord
 from src.run_12ECG_classifier import load_12ECG_model, run_12ECG_classifier
 
-def load_challenge_data(filename):
+def load_challenge_data(filepath: Path):
+    record = rdrecord(filepath)
+    data = record.p_signal.transpose()
 
-    x = loadmat(filename)
-    data = np.asarray(x['val'], dtype=np.float64)
-
-    new_file = filename.replace('.mat','.hea')
-    input_header_file = os.path.join(new_file)
-
-    with open(input_header_file,'r') as f:
+    headerpath = filepath.with_suffix('.hea')
+    with open(headerpath,'r') as f:
         header_data=f.readlines()
-
 
     return data, header_data
 
 
-def save_challenge_predictions(output_directory,filename,scores,labels,classes):
-
-    recording = os.path.splitext(filename)[0]
-    new_file = filename.replace('.mat','.csv')
-    output_file = os.path.join(output_directory,new_file)
+def save_challenge_predictions(output_directory: Path, filename: str, scores,labels,classes):
+    output_file = os.path.join(output_directory / (filename+".csv"))
 
     # Include the filename as the recording number
-    recording_string = '#{}'.format(recording)
+    recording_string = '#{}'.format(filename)
     class_string = ','.join(classes)
     label_string = ','.join(str(i) for i in labels)
     score_string = ','.join(str(i) for i in scores)
@@ -41,22 +34,22 @@ if __name__ == '__main__':
         raise Exception('Include the input and output directories as arguments, e.g., python driver.py model_input model_config input output.')
 
     model_input = sys.argv[1]
-    model_config = sys.argv[2]
-    input_directory = sys.argv[3]
-    output_directory = sys.argv[4]
+    model_config = Path(sys.argv[2])
+    input_directory = Path(sys.argv[3])
+    output_directory = Path(sys.argv[4])
 
     # Find files.
     input_files = []
-    for f in os.listdir(input_directory):
-        if os.path.isfile(os.path.join(input_directory, f)) and not f.lower().startswith('.') and f.lower().endswith('mat'):
-            input_files.append(f)
-
-    if not os.path.isdir(output_directory):
-        os.mkdir(output_directory)
+    for f in input_directory.iterdir():
+        if f.suffix == ".hea":
+            input_files.append(f.stem)
+    
+    if not output_directory.exists():
+        output_directory.mkdir()
 
     # Load model.
     print('Loading 12ECG model...')
-    model = load_12ECG_model(model_input, Path(model_config))
+    model = load_12ECG_model(model_input, model_config)
 
     # Iterate over files.
     print('Extracting 12ECG features...')
@@ -64,8 +57,7 @@ if __name__ == '__main__':
 
     for i, f in enumerate(input_files):
         print('    {}/{}...'.format(i+1, num_files))
-        tmp_input_file = os.path.join(input_directory,f)
-        data,header_data = load_challenge_data(tmp_input_file)
+        data,header_data = load_challenge_data( input_directory / f )
         current_label, current_score,classes = run_12ECG_classifier(data,header_data, model)
         # Save results.
         save_challenge_predictions(output_directory,f,current_score,current_label,classes)
